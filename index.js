709232cc0402c951aa6f5855ef4790c7e5765490
balijawa hussein <balijawahussein@gmail.com>
2024-11-24 17:34:01 +0300

    CREATE : INITIAL COMMIT | IMAGES | FONTS | UI

---
+++ index.js
@@ -0,0 +1,54 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// Navbar animation
+gsap.to('.navbar', {
+    y: 0,
+    duration: 1,
+    ease: 'power4.out'
+});
+
+// Hero section animations
+gsap.to('.hero h1', {
+    opacity: 1,
+    duration: 1,
+    delay: 0.5,
+    y: 30,
+    ease: 'power4.out'
+});
+
+gsap.to('.hero p', {
+    opacity: 1,
+    duration: 1,
+    delay: 0.8,
+    y: 30,
+    ease: 'power4.out',
+    stagger: 0.2
+});
+
+// Mission cards animations
+gsap.to('.card', {
+    scrollTrigger: {
+        trigger: '.mission-cards',
+        start: 'top center',
+        toggleActions: 'play none none reverse'
+    },
+    opacity: 1,
+    y: 0,
+    duration: 0.8,
+    stagger: 0.2,
+    ease: 'power4.out'
+});
+
+// About section animations
+gsap.to('.about h2, .about p', {
+    scrollTrigger: {
+        trigger: '.about',
+        start: 'top center',
+        toggleActions: 'play none none reverse'
+    },
+    opacity: 1,
+    y: 30,
+    duration: 1,
+    stagger: 0.2,
+    ease: 'power4.out'
+});
