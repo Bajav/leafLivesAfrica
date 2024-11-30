743971e172e211a752c97700451d01c420d060e0
balijawa hussein <balijawahussein@gmail.com>
2024-11-30 23:39:40 +0300

    CREATE : CONTACT PAGE

--- index.js
+++ index.js
@@ -52,6 +52,15 @@ gsap.to('.about h2, .about p', {
     stagger: 0.2,
     ease: 'power4.out'
 });
+gsap.to('.about img', {
+    scrollTrigger: {
+        trigger: '.aboutImg',
+        start: 'top bottom',
+        toggleActions: 'play none none reverse'
+    },
+    opacity: 1,
+   scaleX:1
+});
 gsap.to('.project', {
     scrollTrigger: {
         trigger: '.project',
@@ -63,4 +72,31 @@ gsap.to('.project', {
     duration: 1,
     stagger: 0.2,
     ease: 'power4.out'
+});
+// Select the content sections and corresponding images
+const details = gsap.utils.toArray(".project-content");
+const photos = gsap.utils.toArray(".imgContainer");
+
+// Set initial positions for the photos (off-screen)
+gsap.set(photos, { yPercent: 101 })
+
+// Pin the image section while scrolling
+ScrollTrigger.create({
+  trigger: ".projects-container", 
+  start: "top top", 
+  end: "bottom bottom",
+  pin: ".imgSide",
+});
+
+// Animate each image based on the associated header scroll
+details.forEach((detail, index) => {
+  let header = detail.querySelector("h2");
+  ScrollTrigger.create({
+    trigger: header,
+    start: "top 80%",
+    end: "top 70%",
+    animation: gsap.top(photos[index],{yPercent:0}),
+    scrub: true,
+    markers: false
+  });
 });
